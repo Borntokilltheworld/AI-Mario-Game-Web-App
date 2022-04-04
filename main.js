243073a548540ff2430cffe896119486091fc82f
Borntokilltheworld <77736929+Borntokilltheworld@users.noreply.github.com>
2022-04-04 13:25:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
 function preload() {
-	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
 }
 
 function setup() {
 	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
 	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+  video.size(800,400);
+  video.parent('game_console');
+  poseNet = ml5.poseNet(video,ModelLoaded);
+  poseNet.on('pose',gotPoses);
+}
+function draw(){
+   game();
+}
+function ModelLoaded(){
+	console.log("Model Loaded!");
+}
+function gotPoses(results){
+	if(results.length > 0){
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+		console.log("noseX = ");
+	  }
 }
 
-function draw() {
-	game()
-}
 
 
 
